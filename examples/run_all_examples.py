52adc48342a6402f116c57001ed9521af2a13fab
harshlearn001 <harshalbarhate1@gmail.com>
2026-07-15 15:51:31 +0530

    chore(examples): reorganize OI demo examples

---
+++ examples/run_all_examples.py
@@ -0,0 +1,18 @@
+def main():
+
+    print("=" * 70)
+    print("OPTIONFORGE OI EXAMPLES")
+    print("=" * 70)
+
+    demo_summary()
+    demo_by_strike()
+    demo_change()
+    demo_buildup()
+    demo_wall()
+    demo_concentration()
+    demo_shift()
+    demo_trend()
+
+
+if __name__ == "__main__":
+    main()
